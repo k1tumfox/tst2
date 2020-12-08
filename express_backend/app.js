db7f329c94334e13b1e27bcbc8c742dcde9aa017
Tenyson Kwon <tenysonkwon@gmail.com>
2020-12-07 22:21:00 -0500

    login test works

--- express_backend/app.js
+++ express_backend/app.js
@@ -6,10 +6,47 @@ const logger = require('morgan');
 const db = require('./db');
 const dbHelpers = require('./models')(db);
 
-const indexRouter = require('./routes/index');
+// const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const { generateId, authenticateUser, fetchUser } = require('./helpers/loginHelper');
+const bodyParser = require("body-parser");
+
 const app = express();
+
+
+const userDatabaseIshArr = [
+  {
+    nicename: "pollo",
+    fullname: "Pequeño Pollo de la Pampa",
+    email: "pockpock@chicken.com",
+    password: "cluck"
+  }
+  ,
+  {
+    nicename: "dimitri",
+    fullname: "Dimitri Ivanovich Mendeleiv",
+    email: "periodic@table.com",
+    password: "vodka"
+  }
+]
+
+const userDatabaseIshObj = {
+  "pockpock@chicken.com": {
+    nicename: "pollo",
+    fullname: "Pequeño Pollo de la Pampa",
+    email: "pockpock@chicken.com",
+    password: "cluck"
+  }
+  ,
+  "periodic@table.com": {
+    nicename: "dimitri",
+    fullname: "Dimitri Ivanovich Mendeleiv",
+    email: "periodic@table.com",
+    password: "vodka"
+  }
+}
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +58,82 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/api/users', usersRouter(dbHelpers));
 
-// catch 404 and forward to error handler
+//----------------------------------------------------------------------------
+
+app.get("/", (req, res) => {
+  const email = req.cookies.user
+
+  const templateVars = {}
+  const fetchedUser = fetchUser(userDatabaseIshArr, email)
+  if (fetchedUser.error) {
+    templateVars.user = null
+  } else {
+    templateVars.user = fetchedUser.user
+  }
+  res.render("index", templateVars);
+});
+
+// LOGIN ROUTES
+
+app.get('/login', (req, res) => {
+  const templateVars = { error: null }
+  res.render('login', templateVars)
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  // const email = req.body.email
+  // const password = req.body.password
+  const { email, password } = req.body
+  const fetchedUserInfo = authenticateUser(userDatabaseIshArr, email, password)
+
+  if (fetchedUserInfo.error) {
+    console.log(`There was this error : ${fetchedUserInfo.error}`)
+    // return res.redirect('/login')
+    const templateVars = { error: fetchedUserInfo.error }
+    return res.render('login', templateVars)
+  } else {
+    res.cookie('user', fetchedUserInfo.user.email)
+    return res.redirect('/')
+  }
+  // userDatabaseIshObj[email].password === password
+})
+
+// REGISTER ROUTES
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+app.post('/register', (req, res) => {
+  const { email, password, nicename, fullname } = req.body
+
+  const fetchedUser = fetchUser(userDatabaseIshArr, email)
+  if (fetchedUser.error) {
+    const newUser = {
+      nicename,
+      fullname,
+      email,
+      password
+    }
+    userDatabaseIshArr.push(newUser)
+    res.cookie('user', email)
+    res.redirect('/')
+  } else {
+    const templateVars = { error: "Email already exists, please login" }
+    res.render('login', templateVars)
+  }
+
+})
+
+// LOGOUT ROUTE
+app.get('/logout', (req, res) => {
+  res.clearCookie('user')
+  res.redirect('/')
+})
+
+// catch 404 and forward to error handler--------------------------------
 app.use(function(req, res, next) {
   next(createError(404));
 });
--- express_backend/routes/index.js
+++ express_backend/routes/index.js
@@ -1,9 +1,55 @@
+// NOT USING AT THE MOMENT
 var express = require('express');
 var router = express.Router();
 
+const { generateId, authenticateUser, fetchUser } = require('../helpers/loginHelper');
+
+
+const userDatabaseIshArr = [
+  {
+    nicename: "pollo",
+    fullname: "Pequeño Pollo de la Pampa",
+    email: "pockpock@chicken.com",
+    password: "cluck"
+  }
+  ,
+  {
+    nicename: "dimitri",
+    fullname: "Dimitri Ivanovich Mendeleiv",
+    email: "periodic@table.com",
+    password: "vodka"
+  }
+]
+
+const userDatabaseIshObj = {
+  "pockpock@chicken.com": {
+    nicename: "pollo",
+    fullname: "Pequeño Pollo de la Pampa",
+    email: "pockpock@chicken.com",
+    password: "cluck"
+  }
+  ,
+  "periodic@table.com": {
+    nicename: "dimitri",
+    fullname: "Dimitri Ivanovich Mendeleiv",
+    email: "periodic@table.com",
+    password: "vodka"
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const email = req.cookies.user
+
+  const templateVars = {}
+  const fetchedUser = fetchUser(userDatabaseIshArr, email)
+  if (fetchedUser.error) {
+    templateVars.user = null
+  } else {
+    templateVars.user = fetchedUser.user
+  }
+
+  res.render('index', templateVars);
 });
 
 module.exports = router;
